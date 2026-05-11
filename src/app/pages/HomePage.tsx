223707072b3e589077c7ee16cbbadf3fd78935d8
hoainhan.dev <hoainhan.dev@gmail.com>
2026-05-11 08:34:59 +0700

    Remove SpecialOffers component from HomePage
    Removed SpecialOffers component from HomePage.

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
       <main>
         <Hero />
         <MenuSection />
-        <SpecialOffers />
+        
         <StorySection />
         <LocationSection />
       </main>
